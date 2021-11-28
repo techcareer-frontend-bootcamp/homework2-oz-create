32f9c36b9713635e3ff7451dd534c597e3d0ee25
özlem engin <ozlemengin4542@gmail.com>
2021-11-28 20:31:40 +0300

    eksikler tamamlandı, sass dönüşümü yapıldı.

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,21 +5,52 @@ const showMenu = (toggleId, navId) => {
 
     if(toggle && nav) {
         toggle.addEventListener('click', () => {
-            nav.classList.toggle('show')
+            nav.classList.toggle('show-menu')
         })
     }
 }
 
 showMenu('nav-toggle', 'nav-menu')
 
+
 /*==================== REMOVE MOBILE MENU ====================*/
 const navLink = document.querySelectorAll('.nav__link')
 
+
 function linkAction(){
-    const navMenu = document.getElementById('nav-menu')
+    // Active Link
+    navLink.forEach(n => n.classList.remove('active'))
+    this.classList.add('active')
+ 
     // When we click on each nav__link, we remove the show class
-    navMenu.classList.remove('show')
+    const navMenu = document.getElementById('nav-menu') 
+    navMenu.classList.remove('show-menu') 
 }
+
 
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
+
+
+/*==================== Play song ====================*/
+const playSong = (icon, song) => {
+    
+    const mySongIcon = document.getElementById(icon),
+          mySong = document.getElementById(song)
+  
+    if(mySong && mySongIcon) {
+        mySongIcon.addEventListener('click', () => {
+            if(mySong.paused) {
+                mySong.play()
+                mySongIcon.classList.remove('bx-play-circle') 
+                mySongIcon.classList.add('bx-pause-circle') 
+            } else {
+                mySong.pause()
+                mySongIcon.classList.remove('bx-pause-circle') 
+                mySongIcon.classList.add('bx-play-circle') 
+            }
+        })
+    }
+}
+
+playSong('song-icon', 'mySong')
